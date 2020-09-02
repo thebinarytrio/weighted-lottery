6d01a1980223815cdf1964a81b27494ef31d06be
Marianne <mariannetolentino@csu.fullerton.edu>
2020-09-02 10:06:32 -0700

    added comments

--- rest-api-backend/main.go
+++ rest-api-backend/main.go
@@ -20,6 +20,8 @@ var db *sql.DB
 var err error
 
 func main() {
+
+	// connecting to the database
 	db, err = sql.Open("mysql", "root:@/lottery")
 
 	if err != nil {
@@ -28,11 +30,13 @@ func main() {
 
 	defer db.Close()
 
+	// creating new gorilla/mux router
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/users", getUsers).Methods("GET")
 
+	// creating connection to local browser
 	http.ListenAndServe(":30000", router)
 }
 
@@ -41,11 +45,15 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homepage")
 }
 
+// retrieves all users from the users table and response with JSON formatted variables
 func getUsers(w http.ResponseWriter, r *http.Request) {
+
+	// sets content-type to applications/json
 	w.Header().Set("Content-Type", "application/json")
 
 	var users []Users
 
+	// query returns the rows of the database
 	result, err := db.Query("SELECT * FROM `users`")
 	if err != nil {
 		panic(err.Error())
@@ -53,6 +61,8 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 
 	defer result.Close()
 
+	// loop over result and for every iteration, creates new User instance
+	// then scans the result into user object
 	for result.Next() {
 		var user Users
 		err := result.Scan(&user.Userid, &user.Username)
@@ -62,6 +72,7 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 		users = append(users, user)
 	}
 
+	// encodes users array into JSON string
 	json.NewEncoder(w).Encode(users)
 	fmt.Println("Endpoint Hit: getUsers")
 }
